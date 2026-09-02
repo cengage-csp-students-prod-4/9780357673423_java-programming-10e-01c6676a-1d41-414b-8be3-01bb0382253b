fba94a79a8e6a31113bdcbac6774441d1b68a07c
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-02 03:54:21 +0000

    chapter0/ex00 Task3

--- chapter0/ex00/student/Example.java
+++ chapter0/ex00/student/Example.java
@@ -1,6 +1,6 @@
 // This is the code editor
 public class Example {
     public static void main(String[] args) {
-        System.out.println("This is the code editor.");
+        System.out.println("This is a simple activity.");
     }
 }
