dc8ad6cef94788e4f292f5f44835df30bd42b5e9
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-02 04:11:57 +0000

    chapter1/ex07 Task1

--- chapter1/ex07/student/MovieQuoteInfo.java
+++ chapter1/ex07/student/MovieQuoteInfo.java
@@ -1,1 +1,8 @@
-public class MovieQuoteIn
+public class MovieQuoteInfo{
+    public static void main(String[] args) {
+        System.out.println("Rosebud,");
+        System.out.println("said by Charles Foster Kane");
+        System.out.println("in the movie Citizen Kane");
+        System.out.println("in 1941.");
+    }
+} 
