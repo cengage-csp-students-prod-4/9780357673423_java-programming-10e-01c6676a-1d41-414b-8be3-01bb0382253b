efab9d10daa50ffa3f835812e352f0ecdc5983c0
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-07 02:47:02 +0000

    chapter3/ex06 Task1

--- chapter3/ex06/student/InchConversion.java
+++ chapter3/ex06/student/InchConversion.java
@@ -1,1 +1,24 @@
-// Write your code here
+import java.util.Scanner;
+
+public class InchConversion {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter inches >> ");
+        int inches = input.nextInt();
+
+        convertToFeet(inches);
+        convertToYards(inches);
+    }
+
+    public static void convertToFeet(int inches) {
+        double feet = inches / 12.0;
+        System.out.println(inches + " inches is " + feet + " feet");
+    }
+
+    public static void convertToYards(int inches) {
+        double yards = inches / 36.0;
+        System.out.println(inches + " inches is " + yards + " yards");
+    }
+}
