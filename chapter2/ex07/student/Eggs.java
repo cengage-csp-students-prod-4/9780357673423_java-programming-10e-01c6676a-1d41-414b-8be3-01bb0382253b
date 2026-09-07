f8884982a3b2130d6be4123a48b3b83850156fa8
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-07 02:36:08 +0000

    chapter2/ex07 Task1

--- chapter2/ex07/student/Eggs.java
+++ chapter2/ex07/student/Eggs.java
@@ -1,26 +1,18 @@
 public class Eggs {
     public static void main(String[] args) {
-        java.util.Scanner input = new java. util.Scanner(System.in);
+        java.util.Scanner input = new java.util.Scanner(System.in);
 
-        // Prompt user
         System.out.print("Enter the number of eggs: ");
         int eggs = input.nextInt();
 
-        // Prices
-        final double DOZEN_PRICE = 3.25;
-        final double SINGLE_PRICE = 0.45;
-
-        // Calculations
         int dozens = eggs / 12;
         int loose = eggs % 12;
 
-        double total = (dozens * DOZEN_PRICE) + (loose * SINGLE_PRICE);
-        // Display explanation
-        System.out.println("You ordered " + eggs + " eggs.");
-        System.out.println("That's " + dozens + " dozen at $" + DOZEN_PRICE +
-                           " per dozen and " + loose + " loose eggs at " +
-                           SINGLE_PRICE + " cents each for a total of $" +
-                           String.format("%.2f", total) + ".");
-    
+        double total = (dozens * 3.25) + (loose * 0.45);
+
+        System.out.println("You ordered " + eggs + " eggs. That's " +
+                dozens + " dozen at $3.25 per dozen and " +
+                loose + " loose eggs at 45 cents each for a total of $" +
+                String.format("%.2f", total) + ".");
     }
 }
