6da6570452a6ea382953b490a73e1481df8260af
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-07 03:08:03 +0000

    chapter4/ex07 Task1

--- chapter4/ex07/student/TestMonthHandling.java
+++ chapter4/ex07/student/TestMonthHandling.java
@@ -1,1 +1,36 @@
-// Write your code here
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestMonthHandling {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter month >> ");
+        int m1 = input.nextInt();
+        System.out.print("Enter day >> ");
+        int d1 = input.nextInt();
+        System.out.print("Enter year >> ");
+        int y1 = input.nextInt();
+
+        System.out.print("Enter month >> ");
+        int m2 = input.nextInt();
+        System.out.print("Enter day >> ");
+        int d2 = input.nextInt();
+        System.out.print("Enter year >> ");
+        int y2 = input.nextInt();
+
+        LocalDate first = LocalDate.of(y1, m1, d1);
+        LocalDate second = LocalDate.of(y2, m2, d2);
+
+        System.out.println("First date " + first);
+        System.out.println("Second date " + first.plusMonths(1));
+        System.out.println("Third date " + first.plusMonths(2));
+        System.out.println("Fourth date " + first.plusMonths(3));
+
+        System.out.println("First date " + second);
+        System.out.println("Second date " + second.plusMonths(1));
+        System.out.println("Third date " + second.plusMonths(2));
+        System.out.println("Fourth date " + second.plusMonths(3));
+    }
+}
