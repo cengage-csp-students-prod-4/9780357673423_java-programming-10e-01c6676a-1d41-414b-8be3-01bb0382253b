2bb1315cd9a7c5dce4dc7a59737c3e9d6a63a623
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-17 02:59:52 +0000

    chapter6/ex03 Task1

--- chapter6/ex03/student/Factorials.java
+++ chapter6/ex03/student/Factorials.java
@@ -1,1 +1,18 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Factorials {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number to compute factorial >> ");
+        int limit = input.nextInt();
+
+        int factorial = 1;
+
+        for (int i = 1; i <= limit; i++) {
+            factorial = factorial * i;
+            System.out.println("The factorial of " + i + " is " + factorial);
+        }
+    }
+}
