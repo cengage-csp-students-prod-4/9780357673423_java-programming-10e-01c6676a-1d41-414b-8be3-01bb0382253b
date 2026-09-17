7e3691d12969a9399a901df3f50d69a48b7f606a
nolanhudson25 <nolan.hudson@saintleo.edu>
2026-09-17 02:37:26 +0000

    chapter5/ex05 Task6

--- chapter5/ex05/student/PastPresentFuture2.java
+++ chapter5/ex05/student/PastPresentFuture2.java
@@ -1,1 +1,32 @@
-// Write your code here
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class PastPresentFuture2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        LocalDate today = LocalDate.now();
+
+        System.out.print("Enter a month >> ");
+        int month = input.nextInt();
+
+        System.out.print("Enter a day >> ");
+        int day = input.nextInt();
+
+        System.out.print("Enter a year (four digits) >> ");
+        int year = input.nextInt();
+
+        LocalDate entered = LocalDate.of(year, month, day);
+
+        if (entered.isBefore(today)) {
+            System.out.println("The entered date is in the past");
+        } 
+        else if (entered.equals(today)) {
+            System.out.println("The entered date is the current date");
+        } 
+        else if (entered.isAfter(today)) {
+            System.out.println("The entered date is in the future");
+        }
+    }
+}
